128e993f9159e9398ebe318a9bef1d994b9695ad
pranavshastry16 <pranavshastry16@gmail.com>
2026-03-08 23:46:34 -0600

    Finalize Assignment 8 buildroot submission

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -2,16 +2,34 @@
 #
 # aesd-assignments
 #
+# Buildroot package for pulling my assignments-3-and-later repo and installing
+# the Assignment 8 userspace application pieces.
+#
+# For Assignment 8 this package installs:
+#   - /usr/bin/aesdsocket
+#   - /etc/init.d/S99aesdsocket
+#
 ################################################################################
 
-AESD_ASSIGNMENTS_VERSION = cf65db5
+
+AESD_ASSIGNMENTS_VERSION = b9748a0777d2729886233df924a30f2ec856feef
+
+
+# Pull the source from my assignments-3-and-later repo
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-pranavshastry16.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 
+# Build only the server portion, cross-compiling for the Buildroot target
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) -C $(@D)/server CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" all
+	$(MAKE) -C $(@D)/server \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS) -DUSE_AESD_CHAR_DEVICE=1" \
+		LDFLAGS="$(TARGET_LDFLAGS)" \
+		all
 endef
 
+
+# Install the aesdsocket binary and startup script into the target rootfs
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
@@ -19,6 +37,5 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
-
 
 $(eval $(generic-package))
--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-LDD_VERSION = main
-LDD_SITE = https://github.com/cu-ecen-aeld/assignment-7-pranavshastry16.git
+LDD_VERSION = b9748a0777d2729886233df924a30f2ec856feef
+
+LDD_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-pranavshastry16.git
 LDD_SITE_METHOD = git
 
-LDD_MODULE_SUBDIRS = misc-modules scull
+LDD_MODULE_SUBDIRS = aesd-char-driver
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
